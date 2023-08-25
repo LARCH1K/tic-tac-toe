6cba26c97341b75ef3c3e5876c2f551c3f316c52
LARCHIK <ovchinnikov20035696@gmail.com>
2023-08-25 11:24:39 +0500

    Add CommandLineArgumentParser

---
+++ src/main/java/academy/devonline/tictactoe/component/CommandLineArgumentParser.java
@@ -0,0 +1,56 @@
+package academy.devonline.tictactoe.component;
+
+import academy.devonline.tictactoe.model.PlayerType;
+
+import static academy.devonline.tictactoe.model.PlayerType.COMPUTER;
+import static academy.devonline.tictactoe.model.PlayerType.USER;
+
+public class CommandLineArgumentParser {
+
+    private final String[] args;
+
+    public CommandLineArgumentParser(final String[] args) {
+        this.args = args;
+    }
+
+    public PlayerTypes parser(){
+        PlayerType player1Type = null;
+        PlayerType player2Type = null;
+        for (final String arg : args) {
+            if (USER.name().equalsIgnoreCase(arg) || COMPUTER.name().equalsIgnoreCase(arg)) {
+                if (player1Type == null) {
+                    player1Type = PlayerType.valueOf(arg.toUpperCase());
+                } else if (player2Type == null) {
+                    player2Type = PlayerType.valueOf(arg.toUpperCase());
+                } else System.err.println("Unsupported command line argument: '" + arg + "'");
+            } else System.err.println("Unsupported command line argument: '" + arg + "'");
+        }
+        if (player1Type == null) {
+            return new PlayerTypes(USER, COMPUTER);
+        } else if (player2Type == null) {
+            return new PlayerTypes(USER,player1Type);
+        } else {
+            return new PlayerTypes(player1Type, player2Type);
+        }
+    }
+
+    public static class PlayerTypes{
+        private final PlayerType player1Type;
+
+        private final PlayerType player2Type;
+
+        private PlayerTypes(final PlayerType player1Type,
+                            final PlayerType player2Type) {
+            this.player1Type = player1Type;
+            this.player2Type = player2Type;
+        }
+
+        public PlayerType getPlayer2Type() {
+            return player2Type;
+        }
+
+        public PlayerType getPlayer1Type() {
+            return player1Type;
+        }
+    }
+}
