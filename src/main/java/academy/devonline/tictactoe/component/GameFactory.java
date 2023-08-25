5475d5bb5477f2bfdbb9947dead7436b4226912c
LARCHIK <ovchinnikov20035696@gmail.com>
2023-08-25 15:22:24 +0500

    Add GameWindow

--- src/main/java/academy/devonline/tictactoe/component/GameFactory.java
+++ src/main/java/academy/devonline/tictactoe/component/GameFactory.java
@@ -3,6 +3,7 @@ package academy.devonline.tictactoe.component;
 import academy.devonline.tictactoe.component.console.ConsoleDataPrinter;
 import academy.devonline.tictactoe.component.console.ConsoleUserInputReader;
 import academy.devonline.tictactoe.component.keypad.DesktopNumericKeypadCellNumberConverter;
+import academy.devonline.tictactoe.component.swing.GameWindow;
 import academy.devonline.tictactoe.model.Player;
 import academy.devonline.tictactoe.model.PlayerType;
 
@@ -25,9 +26,14 @@ public class GameFactory {
     }
 
     public Game create() {
-        final CellNumberConverter cellNumberConverter = new DesktopNumericKeypadCellNumberConverter();
-        final DataPrinter dataPrinter= new ConsoleDataPrinter(cellNumberConverter);
-        final UserInputReader userInputReader=new ConsoleUserInputReader(cellNumberConverter,dataPrinter);
+
+        final GameWindow gameWindow = new GameWindow();
+        final DataPrinter dataPrinter= gameWindow;
+        final UserInputReader userInputReader=gameWindow;
+
+//        final CellNumberConverter cellNumberConverter = new DesktopNumericKeypadCellNumberConverter();
+//        final DataPrinter dataPrinter= new ConsoleDataPrinter(cellNumberConverter);
+//        final UserInputReader userInputReader=new ConsoleUserInputReader(cellNumberConverter,dataPrinter);
 
         final Player player1;
         if (player1Type == USER) {
