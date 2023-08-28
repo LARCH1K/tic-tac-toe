8db954be9306eb9bdfd6bc9427d4a2cc5d248bb2
LARCHIK <ovchinnikov20035696@gmail.com>
2023-08-28 16:47:08 +0500

    Add PreventUserWinComputerMoveStrategy

--- src/main/java/academy/devonline/tictactoe/model/game/Sign.java
+++ src/main/java/academy/devonline/tictactoe/model/game/Sign.java
@@ -8,6 +8,16 @@ public enum Sign {
 
     EMPTY;
 
+    public Sign oppositeSign() {
+        if (this == X) {
+            return O;
+        } else if (this == O) {
+            return X;
+        } else {
+            throw new IllegalStateException("Empty Value does not have an opposite one");
+        }
+    }
+
     @Override
     public String toString() {
         if (this == EMPTY) {
