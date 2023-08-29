251277650a2a524f8b129f55a1b08759472d2920
LARCHIK <ovchinnikov20035696@gmail.com>
2023-08-29 10:27:27 +0500

    Add WinOnTheNextStepComputerMoveStrategy

--- src/main/java/academy/devonline/tictactoe/component/strategy/PreventUserWinComputerMoveStrategy.java
+++ src/main/java/academy/devonline/tictactoe/component/strategy/PreventUserWinComputerMoveStrategy.java
@@ -3,6 +3,10 @@ package academy.devonline.tictactoe.component.strategy;
 import academy.devonline.tictactoe.model.game.Sign;
 
 public class PreventUserWinComputerMoveStrategy extends AbstractComputerMoveStrategy {
+    public PreventUserWinComputerMoveStrategy() {
+        super(1);
+    }
+
     @Override
     protected Sign getFindSign(final Sign moveSign) {
         return moveSign.oppositeSign();
--- src/main/java/academy/devonline/tictactoe/component/strategy/WinOnTheNextStepComputerMoveStrategy.java
+++ src/main/java/academy/devonline/tictactoe/component/strategy/WinOnTheNextStepComputerMoveStrategy.java
@@ -2,9 +2,9 @@ package academy.devonline.tictactoe.component.strategy;
 
 import academy.devonline.tictactoe.model.game.Sign;
 
-public class WinNowComputerMoveStrategy extends AbstractComputerMoveStrategy {
-    public WinNowComputerMoveStrategy() {
-        super(1);
+public class WinOnTheNextStepComputerMoveStrategy extends AbstractComputerMoveStrategy {
+    public WinOnTheNextStepComputerMoveStrategy() {
+        super(2);
     }
 
     @Override
